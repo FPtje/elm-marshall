60605e56a47bcc9b31840c10c65ee6cd2b979c4f
Falco Peijnenburg <falco@lumiguide.nl>
2017-02-05 21:28:55 +0100

    Fixed infinite loop and undefined errors

--- src/Elm/Marshall/Internal/Class.hs
+++ src/Elm/Marshall/Internal/Class.hs
@@ -43,19 +43,18 @@ class ElmMarshall a where
   toElm x = ElmValue <$> (genericToElm $ from x)
 
   default fromElm :: (Generic a, GenericElmMarshall (Rep a)) => ElmValue a -> IO a
-  fromElm x = genericFromElm (from (undefined :: a)) (unElmValue x) >>= pure . to
+  fromElm x = genericFromElm (unElmValue x) >>= pure . to
 
 
 class GenericElmMarshall f where
   genericToElm :: f a -> IO JSVal
 
-  -- the f a here can be undefined
-  genericFromElm :: f a -> JSVal -> IO (f a)
+  genericFromElm :: JSVal -> IO (f a)
 
 instance (Datatype d, GenericElmMarshall f) =>
          GenericElmMarshall (D1 d f) where
   genericToElm datatype = genericToElm $ unM1 datatype
-  genericFromElm datatype jsv = M1 <$> genericFromElm (unM1 datatype) jsv
+  genericFromElm jsv = M1 <$> genericFromElm jsv
 
 instance (Constructor c, GenericElmMarshallSelector f) =>
          GenericElmMarshall (C1 c f) where
@@ -67,12 +66,11 @@ instance (Constructor c, GenericElmMarshallSelector f) =>
 
       pure $ jsval obj
 
-  genericFromElm constructor jsv =
-      M1 <$> genericFromElmSelector (unM1 constructor) (Object jsv)
+  genericFromElm jsv = M1 <$> genericFromElmSelector (Object jsv)
 
 class GenericElmMarshallSelector f where
   genericToElmSelector :: f a -> IO [(String, JSVal)]
-  genericFromElmSelector :: f a -> Obj.Object -> IO (f a)
+  genericFromElmSelector :: Obj.Object -> IO (f a)
 
 instance (Selector ('MetaSel ('Just u) v w x), GenericElmMarshall a) =>
          GenericElmMarshallSelector (S1 ('MetaSel ('Just u) v w x) a) where
@@ -82,11 +80,11 @@ instance (Selector ('MetaSel ('Just u) v w x), GenericElmMarshall a) =>
           val <- genericToElm $ unM1 selector
           pure [(name, val)]
 
-  genericFromElmSelector selector obj =
-      case selName selector of
+  genericFromElmSelector obj =
+      case selName (undefined :: t ('MetaSel ('Just u) v w x) a p) of
         name -> do
           prop <- Obj.getProp (fromString name) obj
-          val <- genericFromElm (unM1 selector) prop
+          val <- genericFromElm prop
           pure $ M1 val
 
 
@@ -95,14 +93,15 @@ instance (GenericElmMarshallSelector f, GenericElmMarshallSelector g) =>
   genericToElmSelector (l :*: r) =
       (++) <$> genericToElmSelector l <*> genericToElmSelector r
 
-  genericFromElmSelector (l :*: r) obj =
-      (:*:) <$> genericFromElmSelector l obj <*> genericFromElmSelector r obj
+  genericFromElmSelector obj =
+      (:*:) <$> genericFromElmSelector obj <*> genericFromElmSelector obj
 
 
 instance ElmMarshall a => GenericElmMarshall (Rec0 a) where
   genericToElm rec = unElmValue <$> (toElm $ unK1 rec)
 
-  genericFromElm rec val = K1 <$> fromElm (ElmValue val)
+  genericFromElm val =
+    K1 <$> fromElm (ElmValue val)
 
 --------------
 
@@ -125,7 +124,8 @@ instance ElmMarshall Int where
 
 instance {-# OVERLAPPING #-} ElmMarshall String where
   toElm     = fmap ElmValue . toJSVal . pack
-  fromElm x = fromJSValUnchecked (unElmValue x) >>= fromJSValUnchecked
+  fromElm x =
+    fromJSValUnchecked (unElmValue x) >>= fromJSValUnchecked
 
 instance ElmMarshall T.Text where
   toElm   = fmap ElmValue . toJSVal . textToJSString
@@ -147,7 +147,7 @@ instance ElmMarshall a => ElmMarshall (Maybe a) where
       if isNull $ unElmValue x then
         pure Nothing
       else
-        fromElm x
+        Just <$> fromElm (ElmValue $ unElmValue x)
 
 instance ElmMarshall JSVal where
   toElm = pure . ElmValue
